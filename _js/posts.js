badc862058d30357c80b13b23d0e4d8a28953463
BibleFarm <smuglovsky@gmail.com>
2019-10-17 13:24:21 -0300

    .

--- _js/posts.js
+++ _js/posts.js
@@ -144,130 +144,113 @@ $("title").text(document.title + " - BibleFarm.org");
 			window.history.back();
 	});
 	// END done_with_ready_post_close_tab
-	////////////////////////////////////////////////////
-	// BEGIN PureChat hack
-	////////////////////////////////////////////////////
-	$(document).ready(function() {
-		// initial hide
-		var stopWaitingForPurechat = setInterval(function() {
-			if ($("body").find('.purechat-widget-title-link:contains("Contact us")').length > 0) {
-				console.log("found PureChat");
-				// hide it
-				$("#PureChatWidget").attr("style", "z-index: -1 !important");
-				clearInterval(stopWaitingForPurechat);
-			}
-		}, 1000);
-		////////////////////
-		// show when needed
-		$("#show_pure_chat, .show_pure_chat").on("click", function() {
-			// show it
-			$("#PureChatWidget").attr("style", "z-index: 2147483646 !important");
-			// hack colors
-			// header
-			$("#PureChatWidget").find(".purechat-widget-header").attr("style", "background-color: #0c1400 !important");
-			// content-wrapper
-			$("#PureChatWidget").find(".purechat-content-wrapper").attr("style", "background-color: #172800 !important");
-			// purechat-card
-			$("#PureChatWidget").find(".purechat-card").attr("style", "background-color: #203700 !important");
-			// purechat-btn
-			$("#PureChatWidget").find(".purechat-btn").attr("style", "color: #d5ad6d !important");
-			// purechat-widget-title-link
-			$("#PureChatWidget").find(".purechat-widget-title-link").attr("style", "font-size: 20px !important;padding-left: 8px !important;font-family: PT_Sans-Narrow-Web-Regular !important;color: #d5ad6d !important;text-shadow: 2px 2px 1px black !important");
-			// keep widget expanded
-			$("#PureChatWidget").removeClass("purechat-widget-super-collapsed");
-			$("#PureChatWidget").addClass("purechat-widget-collapsed");
-			console.log("PureChat will keep widget expanded");
-			setTimeout(function() {
-				$("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
-			}, 1000);
-			setTimeout(function() {
-				$("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
-			}, 1500);
-			setTimeout(function() {
-				$("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
-			}, 2000);
-			setTimeout(function() {
-				$("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
-			}, 2500);
-			setTimeout(function() {
-				$("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
-			}, 3000);
-			setTimeout(function() {
-				$("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
-			}, 3500);
-			setTimeout(function() {
-				$("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
-			}, 4000);
-
-// create dynamically the element
-$("body").one().append('<div class="opacity_cover"></div>');
-console.log("create dynamically opacity_cover element");
-      // darken the page to emphasize chat
-      $(".opacity_cover").show();
-
-
-
-		});
-
-
+  ////////////////////////////////////////////////////
+  // BEGIN PureChat hack
+  ////////////////////////////////////////////////////
+  $(document).ready(function() {
+    // initial hide
+    var stopWaitingForPurechat = setInterval(function() {
+      if ($("body").find('.purechat-widget-title-link:contains("Contact us")').length > 0) {
+        console.log("found PureChat");
+        // hide it
+        $("#PureChatWidget").attr("style", "z-index: -1 !important");
+        clearInterval(stopWaitingForPurechat);
+      }
+    }, 1000);
+    ////////////////////
+    // show when needed
+    $(".open_purechat").on("click", function() {
+      // show it
+      $("#PureChatWidget").attr("style", "z-index: 2147483646 !important");
+      // hack colors
+      // header
+      $("#PureChatWidget").find(".purechat-widget-header").attr("style", "background-color: #0c1400 !important");
+      // content-wrapper
+      $("#PureChatWidget").find(".purechat-content-wrapper").attr("style", "background-color: #172800 !important");
+      // purechat-card
+      $("#PureChatWidget").find(".purechat-card").attr("style", "background-color: #203700 !important");
+      // purechat-btn
+      $("#PureChatWidget").find(".purechat-btn").attr("style", "color: #d5ad6d !important");
+      // purechat-widget-title-link
+      $("#PureChatWidget").find(".purechat-widget-title-link").attr("style", "font-size: 20px !important;padding-left: 8px !important;font-family: PT_Sans-Narrow-Web-Regular !important;color: #d5ad6d !important;text-shadow: 2px 2px 1px black !important");
+      // keep widget expanded
+      $("#PureChatWidget").removeClass("purechat-widget-super-collapsed");
+      $("#PureChatWidget").addClass("purechat-widget-collapsed");
+      console.log("PureChat will keep widget expanded");
+      setTimeout(function() {
+        $("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
+      }, 1000);
+      setTimeout(function() {
+        $("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
+      }, 1500);
+      setTimeout(function() {
+        $("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
+      }, 2000);
+      setTimeout(function() {
+        $("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
+      }, 2500);
+      setTimeout(function() {
+        $("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
+      }, 3000);
+      setTimeout(function() {
+        $("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 50px #b3ff00 !important");
+      }, 3500);
+      setTimeout(function() {
+        $("body").find("#PureChatWidget.purechat.purechat-widget-collapsed").attr("style", "box-shadow: 0px 0px 10px #b3ff00 !important");
+      }, 4000);
+      // create dynamically the element
+      $("body").one().append('<div class="opacity_cover"></div>');
+      console.log("create dynamically opacity_cover element");
+            // darken the page to emphasize chat
+            $(".opacity_cover").show();
+    });
     // hide when click outside
     $(".opacity_cover").click(function() {
       // hide it
       $("#PureChatWidget").attr("style", "z-index: -1 !important");
       // bring back to normal
       $(".opacity_cover").hide();
-      console.log("hide dynamically opacity_cover element");
     });
+    // stopPropagation when clicking to show the widget and inside the widget itself
+    $(".open_purechat, #PureChatWidget").click(function(event) {
+      event.stopPropagation();
+      console.log("PureChat hide when click outside event.stopPropagation");
+    });
+  });
+  ////////////////////////////////////////////////////
+  // END PureChat hack
+  ////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////
+  // BEGIN PureChat load
+  ////////////////////////////////////////////////////
+  window.purechatApi = {
+    l: [],
+    t: [],
+    on: function() {
+      this.l.push(arguments);
+    }
+  };
+  (function() {
+    var done = false;
+    var script = document.createElement('script');
+    script.async = true;
+    script.type = 'text/javascript';
+    script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript';
+    document.getElementsByTagName('HEAD').item(0).appendChild(script);
+    script.onreadystatechange = script.onload = function(e) {
+      if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+        var w = new PCWidget({
+          c: '7f1570e3-a978-430a-82a3-fccff55a8707',
+          f: true
+        });
+        done = true;
+      }
+    };
+  })();
+  ////////////////////////////////////////////////////
+  // END PureChat load
+  ////////////////////////////////////////////////////
 
-
-
-		// hide when click outside
-		$(window).click(function() {
-			// hide it
-			$("#PureChatWidget").attr("style", "z-index: -1 !important");
-		});
-		// stopPropagation when clicking to show the widget and inside the widget itself
-		$("#show_pure_chat, #PureChatWidget").click(function(event) {
-			event.stopPropagation();
-			console.log("PureChat hide when click outside event.stopPropagation");
-		});
-	});
-	////////////////////////////////////////////////////
-	// END PureChat hack
-	////////////////////////////////////////////////////
-
-
-
-	////////////////////////////////////////////////////
-	// BEGIN PureChat load
-	////////////////////////////////////////////////////
-	window.purechatApi = {
-		l: [],
-		t: [],
-		on: function() {
-			this.l.push(arguments);
-		}
-	};
-	(function() {
-		var done = false;
-		var script = document.createElement('script');
-		script.async = true;
-		script.type = 'text/javascript';
-		script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript';
-		document.getElementsByTagName('HEAD').item(0).appendChild(script);
-		script.onreadystatechange = script.onload = function(e) {
-			if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-				var w = new PCWidget({
-					c: '7f1570e3-a978-430a-82a3-fccff55a8707',
-					f: true
-				});
-				done = true;
-			}
-		};
-	})();
-	////////////////////////////////////////////////////
-	// END PureChat load
-	////////////////////////////////////////////////////
 
 
 
